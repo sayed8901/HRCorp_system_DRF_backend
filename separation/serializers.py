a355f232afefbac0326eca30dbe76a535d56fb64
sayed8901 <sayed91515@gmail.com>
2024-09-03 01:06:16 +0600

    Separation serializer modified.

--- separation/serializers.py
+++ separation/serializers.py
@@ -1,10 +1,14 @@
 from rest_framework import serializers
 from .models import SeparationInfo
 
+
 class SeparationInfoSerializer(serializers.ModelSerializer):
     class Meta:
         model = SeparationInfo
-        fields = '__all__'
+
+        fields = ['employee', 'application_submission_date', 'separation_effect_date', 'cause_of_separation', 'separation_type', ]
+
+        # fields = '__all__'
         # exclude = ['employee',]
 
         read_only_fields = ['employee', ]
