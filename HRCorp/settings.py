e1865cd717687009ebb6a95c7d85fdb6120ca4e5
sayed8901 <sayed91515@gmail.com>
2024-10-03 14:27:58 +0600

    vercel.json modified

--- HRCorp/settings.py
+++ HRCorp/settings.py
@@ -45,15 +45,15 @@ ALLOWED_HOSTS = ["*"]
 
 
 # allowing origins for accessing from our API
-CORS_ALLOW_ALL_ORIGINS = False  # Set this to False to restrict origins
+CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = [
-    'http://127.0.0.1:8000',        # Local backend
-    'http://localhost:8000',        # Local backend
-    'https://hr-corp-system-drf-backend.vercel.app',  # Backend on Vercel
-    'http://localhost:5173',        # frontend's localhost
-    'https://hrcorp.netlify.app',   # deployed frontend
-]
+# CORS_ALLOWED_ORIGINS = [
+#     'http://127.0.0.1:8000',        # Local backend
+#     'http://localhost:8000',        # Local backend
+#     'https://hr-corp-system-drf-backend.vercel.app',  # Backend on Vercel
+#     'http://localhost:5173',        # frontend's localhost
+#     'https://hrcorp.netlify.app',   # deployed frontend
+# ]
 
 CORS_ALLOW_METHODS = [
     "GET",
@@ -69,6 +69,8 @@ CORS_ALLOW_HEADERS = [
     "content-type",
     "x-csrftoken",
     "x-requested-with",
+    "accept",
+    "accept-encoding",
 ]
 
 
